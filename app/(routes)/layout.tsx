eafd8939b4709e075a58c2b57c98f4104f8b2109
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-17 08:48:34 +0530

    fix: sidebar for mobile

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -2,12 +2,12 @@ import Sidebar from "@/components/sidebar/sidebar";
 
 const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
-      <div className="hidden md:flex h-full w-72 flex-col fixed inset-y-0 z-50">
+    <div className="h-full flex flex-col md:flex-row">
+      <div className="flex h-full w-full md:w-72 flex-col md:fixed inset-y-0 md:z-50">
         <Sidebar />
       </div>
 
-      <div className="h-full md:max-w-screen-2xl md:mx-auto">
+      <div className="h-full w-full md:max-w-screen-2xl md:mx-auto">
         <div className="pl-72">{children}</div>
       </div>
     </div>
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return ( 
-    <div>
-      HomePage
+    <div className="px-6 py-20">
+      hello
     </div>
    );
 }
