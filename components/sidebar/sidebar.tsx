0cc92d18d8ed4b5c39da325761eec1e549a2b8a8
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-17 08:51:52 +0530

    fix: sidebar items

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -4,13 +4,17 @@ import SidebarRoutes from "./sidebar-routes";
 
 const Sidebar = () => {
   return (
-    <div className="h-full flex flex-col overflow-y-auto shadow-sm border-r-[0.1px] bg-[#1F1F1F]/30 backdrop-blur-md px-2 py-16">
+    <div className="h-full flex flex-col justify-between overflow-y-auto shadow-sm border-r-[0.1px] bg-[#1F1F1F]/30 backdrop-blur-md px-2 py-6">
       <div className="px-6 py-3 w-full flex items-center justify-center">
         <Logo />
       </div>
 
       <div className="flex flex-col w-full">
         <SidebarRoutes />
+      </div>
+
+      <div className="text-xs text-zinc-600 text-center">
+        © 2024 Tanay Vaswani. All rights reserved.
       </div>
     </div>
   );
