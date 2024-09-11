372d770418ad17cadce85b00fc40c904eb48b9b3
tanayvaswani <vaswani.tanay9@gmail.com>
2024-09-11 20:23:51 +0530

    chore: text change

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -6,8 +6,9 @@ const HeroSection = () => {
     <div className="flex flex-col items-start w-full gap-6">
       <div className="flex flex-col-reverse md:flex-row items-center md:items-start justify-between gap-6 w-full">
         <p className="">
-          I’m Tanay Vaswani, an undergrad with a passion for Software
-          Engineering, Machine Learning Systems & Infrastructure at scale.
+          I’m Tanay Vaswani, a photographer and an undergrad with a passion for
+          Software Engineering, Machine Learning Systems & Infrastructure at
+          scale.
           <br />
           <br />
           I’m currently at{" "}
@@ -18,13 +19,13 @@ const HeroSection = () => {
           >
             TurboML, Inc.
           </Link>{" "}
-          working on a Machine Learning Platform Reinvinted for Real-Time.
-          Previously, I used to contribute in commercial & free Open-Source
+          working on a Machine Learning Platform Reinvented for Real-Time.
+          Previously, I used to contribute in commercial & free open-source
           projects.
           <br />
           <br />
           I’ve also served as DevOps Mentor at Google Developer Groups (MAIT,
-          New Delhi) where I taught everything deploying and scaling.
+          New Delhi) where I taught everything about deploying and scaling.
         </p>
 
         <Image
