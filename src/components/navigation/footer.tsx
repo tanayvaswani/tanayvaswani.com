bf59a9c171cfab96899d4003db6d909efe400279
tanayvaswani <vaswani.tanay9@gmail.com>
2024-09-11 23:11:22 +0530

    chore: footer

---
+++ src/components/navigation/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full border-t">
+      <div className="md:max-w-screen-lg md:mx-auto px-4 md:px-0">
+
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
